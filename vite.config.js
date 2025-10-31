f7b51af171bb24a0df3df7f85d7825e86effc18e
John Dawood Soliman <johndawood50@gmail.com>
2025-11-01 01:19:35 +0200

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-tours",
+  base: "/",
   plugins: [react()],
    build: {
     chunkSizeWarningLimit: 1600, // ← زوّد الرقم هنا (القيمة الافتراضية 500)
